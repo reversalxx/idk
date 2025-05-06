02115885f3d2735d90e5f8f6d1614369e00768b5
Reversal?? <160512015+reversalxx@users.noreply.github.com>
2025-05-06 21:47:00 +0600

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.json(__dirname, 'shit')));
+
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'shit', 'z.html'));
+})
 
 // This should be POST, not GET
 app.post('/bypass', async (req, res) => {
